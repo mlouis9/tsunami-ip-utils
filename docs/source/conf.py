47d62b6af9c06820547a94a0b4516c2388330eff
Matthew Louis <mlouis9@gatech.edu>
2024-07-07 13:51:25 -0400

    Changed theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,8 @@ release = '0.0.1'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
@@ -29,5 +30,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+html_theme = 'spinx_rtd_theme'
 html_static_path = ['_static']
