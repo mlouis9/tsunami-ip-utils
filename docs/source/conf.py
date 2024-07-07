78279bbe8ed12e41cf1849e2c5445a133874fb6d
Matthew Louis <mlouis9@gatech.edu>
2024-07-07 18:33:20 -0400

    Added logic for suppressing duplicate object warnings during sphinx build, and chagned theme to sphinx readthedocs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,7 @@
 import os, sys
+import logging
+from docutils.parsers.rst import Directive
+
 sys.path.insert(0, os.path.abspath('../../src'))
 
 # Configuration file for the Sphinx documentation builder.
@@ -20,7 +23,8 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
+    'sphinx_autodoc_typehints'
 ]
 
 templates_path = ['_templates']
@@ -33,14 +37,20 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+# -- Custom Class for Filtering Duplicate Object Warnings --------------------
+"""NOTE: These warnings could not be suppressed using the sphinx suppress_warnings option,
+so this manual approach was necessary. NOTE: also that although the .. automodule:: my_module directive is
+used twice for each module (once for private API and once for public API), there is no overlap in the index because
+the methods/classes in the private and public APIs are mutually exclusive."""
+
+class FilterDuplicateObjectWarnings(logging.Filter):
+    def filter(self, record):
+        is_duplicate_warning = 'duplicate object description of %s, other instance in %s, use :no-index: for one of them' in record.msg
+        return not is_duplicate_warning
 
 # -- Custom Configuration For Generating Public and Private API Docs --------
-from docutils.parsers.rst import Directive
 
 # Suppress warnings from documenting the same module twice (public and private)
-suppress_warnings = [
-    'automodule.duplicate',
-]
 
 class ApiTypeDirective(Directive):
     has_content = False
@@ -55,6 +65,10 @@ def setup(app):
     app.add_config_value('api_type', 'public', 'env')
     app.add_directive('api_type', ApiTypeDirective)
     app.connect('autodoc-skip-member', skip_member)
+
+    # Add filter to Sphinx logger to suppress duplicate object warnings
+    logger = logging.getLogger('sphinx')
+    logger.addFilter(FilterDuplicateObjectWarnings())
 
 def skip_member(app, what, name, obj, skip, options):
     api_type = app.config.api_type
